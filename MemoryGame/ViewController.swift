aa7679899ce2e20b954fe73cf823185500137d26
Ray Kang <adamck6302438@gmail.com>
2019-09-21 22:23:30 -0400

    Initial Commit

---
+++ MemoryGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by Frances ZiyiFan on 9/21/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
